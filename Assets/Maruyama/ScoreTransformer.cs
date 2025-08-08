3fff4b10cd1130584dd59a03532f631e51bbf7d1
mkgi16cd <mkgi16cd@gmail.com>
2025-08-08 13:12:51 +0900

    [fix] keyCountをscoreに変換する.csを仮作成

--- Assets/Maruyama/ScoreTransformer.cs
+++ Assets/Maruyama/ScoreTransformer.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class ScoreTransformer : MonoBehaviour
 {
-    [SerializeField] float _inputCount;
+    [SerializeField] float _scale;
+    public float _count; // 受け取る変数
+    float _distance; // 結果用の箱
+    /// <summary>
+    /// scoreTransformer._count = 作ってもらった変数;　このセットはどっちのコードでも行けるように
+    /// </summary>
+
+    private void Start()
+    {
+        _distance = _count * _scale;
+        Debug.Log("SCORE" + _distance + " KM");
+    }
 }
