4bd6362e328013fdb04af1495a8cb4e5a2601409
SosukeKato <dokindokinpiano@gmail.com>
2025-08-08 12:43:17 +0900

    [Update]_rbでRigidbody2Dを呼び出す処理を追加

--- Assets/Kato/Script/CharacterImpulse.cs
+++ Assets/Kato/Script/CharacterImpulse.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class CharacterImpulse : MonoBehaviour
 {
+    Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
